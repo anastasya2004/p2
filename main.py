e5703ff5a19b8540deec9a848f07b81fe74c26ea
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-03-09 07:46:40 +0300

    main function

--- main.py
+++ main.py
@@ -4,21 +4,84 @@ Group:
 Makeeva Angelina
 Kareva Alena
 Osokina Anastasya
-cvcvc
 '''
 
-def  (x, y, a, b, angle, color):
+import turtle
+def square (x, y, a):
     '''
- frdftgh
- :param x:
- :param y:
- :param a:
- :param b:
- :param angle:
- :param color:
+    Function, drawing square.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a square
+    :return: None
     '''
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.pensize(2)
+    turtle.pencolor ('white')
+    turtle.color('#ff7c00')
+    turtle.speed(100)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.end_fill()
 
 
+def parallelogram(x, y, a, c):
+    '''
+    Function, drawing parallelogram.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: long side of a parallelogram
+    :param c: short side of a parallelogram
+    :return: None
+    '''
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.pensize(2)
+    turtle.pencolor ('white')
+    turtle.color('#8ecc23')
+    turtle.speed(100)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(c)
+    turtle.right(45)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(c)
+    turtle.end_fill()
 
-if == == "_main_":
-    main()
+
+def triangle (x, y, a, b, c):
+    '''
+    Function, drawing triangle.
+    :param x: right angle coordinate x
+    :param y: right angle coordinate y
+    :param a: leg of a triangle
+    :param b: hypotenuse of triangle
+    :param c: color of triangle
+    :return: None
+    '''
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.pensize(2)
+    turtle.pencolor ('white')
+    turtle.color(c)
+    turtle.speed(100)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(135)
+    turtle.end_fill()
+    turtle.forward(a)
