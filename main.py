21b223d9fb0c406846afffeb121e90d4e093a7e3
anastasya2004 <62603182+anastasya2004@users.noreply.github.com>
2023-03-09 11:11:32 +0700

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+'''
+Title
+Group:
+Makeeva Angelina
+Kareva Alena
+'''
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def  (x, y, a, b, angle, color):
+    '''
+ frdftgh
+ :param x:
+ :param y:
+ :param a:
+ :param b:
+ :param angle:
+ :param color:
+    '''
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if == == "_main_":
+    main()
